d4e924af838d4ffc276b4d2d623fc72b3e814f30
EpicJimmy5 <theclone1000@gmail.com>
2019-05-11 21:47:26 -0700

    Music Bot Test v3.3 Fix

--- index.js
+++ index.js
@@ -190,7 +190,7 @@ bot.on("message", async message => {
         filter: 'audioonly',
         quality: 'highestaudio'
       });
-      const dispatcher = connection.playOpusStream(await ytdl(url));
+      const dispatcher = connection.playOpusStream(ytdl(url), streamOptions);
       dispatcher.on("end", end => {
         console.log("left channel");
         voiceChannel.leave();
