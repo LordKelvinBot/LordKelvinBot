ad14e959d4597c18d2bfa247773907dbc614f7a8
EpicJimmy5 <theclone1000@gmail.com>
2020-07-17 17:57:04 -0700

    Build 3b.01

--- index.js
+++ index.js
@@ -1008,9 +1008,13 @@ bot.on("message", async message => {
         var embed10 = new Discord.RichEmbed()
           .setTitle("Developers")
           .setDescription("These are the Developers")
-          .setAuthor('Eddie Vaughn','https://cdn.discordapp.com/attachments/684671474010947609/732567752518271016/DSC05763.png','https://eddiedoesntexistyet.com')
-          .setAuthor('Kyle Chau','https://i.imgur.com/9Qs4rex.jpg','https://byle.dev')
+          .addField("Role","Original Bot Developer")
+          .setAuthor('Eddie Vaughn', 'https://cdn.discordapp.com/attachments/684671474010947609/732567752518271016/DSC05763.png', 'https://eddiedoesntexistyet.com')
+        var embed11 = new Discord.RichEmbed()
+          .addField("Role","Hoster & Part-Time Developer")
+          .setAuthor('Kyle Chau', 'https://i.imgur.com/9Qs4rex.jpg', 'https://byle.dev')
         message.channel.send(embed10);
+        message.channel.send(embed11);
       break;
     case "help":
       if (!args[1]) {
