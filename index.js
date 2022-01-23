73c9d177478f9ab94be50e40f6e0329319c454fb
EpicJimmy5 <8009856+TheKyleChau@users.noreply.github.com>
2022-01-22 20:10:11 -0800

    Purge test.

--- index.js
+++ index.js
@@ -1337,7 +1337,7 @@ bot.on("message", async message => {
         let messagecount = newamount.toString();
         message.channel.messages
           .fetch({
-            limit: parseInt(messagecount)
+            limit: parseInt(messagecount)+1
           })
           .then(messages => {
             message.channel.bulkDelete(messages, true);
