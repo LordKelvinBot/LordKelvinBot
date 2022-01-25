25d22b23774d01cb844967d3da787fc90afae4b7
Byle <8009856+TheKyleChau@users.noreply.github.com>
2022-01-25 14:40:21 -0800

    Redo uptime command.

--- index.js
+++ index.js
@@ -577,8 +577,15 @@ bot.on("message", async message => {
       }
       break;
     case "uptime":
-      var duration = moment.duration(Client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
-      message.channel.send(duration);
+      let totalSeconds = (client.uptime / 1000);
+      let days = Math.floor(totalSeconds / 86400);
+      totalSeconds %= 86400;
+      let hours = Math.floor(totalSeconds / 3600);
+      totalSeconds %= 3600;
+      let minutes = Math.floor(totalSeconds / 60);
+      let seconds = Math.floor(totalSeconds % 60);
+      let uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
+      message.channel.send(uptime);
       break;
     case "ping":
       const m = await message.channel.send("Ping?");
