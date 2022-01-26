a36ecdf06e270889a9d2c59b2767d0d23703ea9e
EpicJimmy5 <8009856+TheKyleChau@users.noreply.github.com>
2022-01-25 21:15:13 -0800

    fix again?

--- index.js
+++ index.js
@@ -1454,7 +1454,7 @@ bot.on("message", async message => {
         let messagecount = newamount.toString();
         message.channel.messages
           .fetch({
-            limit: parseInt(messagecount)+1
+            limit: parseInt(messagecount)
           })
           .then(messages => {
             message.channel.bulkDelete(messages, true);
