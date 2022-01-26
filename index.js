67f6f1041e3198c23ba664401b66535fefff2d5a
EpicJimmy5 <8009856+TheKyleChau@users.noreply.github.com>
2022-01-25 19:40:41 -0800

    Revert to old reset command.

--- index.js
+++ index.js
@@ -598,10 +598,9 @@ bot.on("message", async message => {
       break;
     case "reset":
       if(message.guild.members.cache.get('181284528793452545')) {
-        let resetman = parseInt(args[1]);
         if(args[1]) {
-          console.log("Balance has been reset for player " + resetman);
-          let resetperson = './playerdata/' + resetman + '.json';
+          console.log("Balance has been reset for player " + args[1]);
+          let resetperson = './playerdata/' + args[1] + '.json';
           let newdata = {
             money: 100
           };
