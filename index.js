2eb63a7c6e3d9486c2ce62041cc0c238e9022ab4
Byle <8009856+TheKyleChau@users.noreply.github.com>
2022-01-28 18:18:41 -0800

    Fix

--- index.js
+++ index.js
@@ -336,8 +336,7 @@ bot.on("message", async message => {
   }
   function exchangeMoney(id, exchangeAmount) {
     //message.guild.members.cache.get('181284528793452545')
-    var kyle = '181284528793452545';
-    bot.users.cache.get(kyle).send(id + " wants to redeem " + exchangeAmount);
+    bot.users.cache.get('181284528793452545').send(id + " wants to redeem " + exchangeAmount);
   }
   //will/is/are/am/was/does/should/do/can
   async function slots(amount, id) {
