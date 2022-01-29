dc57f1c170000ce50ec49fca9561777774b47d22
Byle <8009856+TheKyleChau@users.noreply.github.com>
2022-01-28 18:09:34 -0800

    Money withdrawal finish?

--- index.js
+++ index.js
@@ -741,6 +741,7 @@ bot.on("message", async message => {
       if (amount <= 1000000) return message.channel.send("Input a valid number more than 1000000.")
       if (brokeCheck(messageAuthor, amount)) return message.channel.send("You don't have enough money to do that.");
       exchangeMoney(message.author.id, amount)
+      return message.channel.send(amount " has been redeemed.")
       break;
     case "stats":
       convert(messageAuthor);
