87c91cfbd9b24a5655c636f290584443f900eb7a
Byle <8009856+TheKyleChau@users.noreply.github.com>
2022-04-26 16:10:43 -0700

    test translater of deepl

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ const dt = require('date-fns/toDate');
 const wt = require('weather-js');
 const moment = require("moment");
 const blackjack = require("discord-blackjack");
+const https = require('https');
 require("moment-duration-format");
 
 
@@ -73,6 +74,7 @@ const configuration = new Configuration({
   apiKey: AITOKEN,
 });
 const openai = new OpenAIApi(configuration);
+const DEEPLT = config.DEEPLTOKEN;
 const TOKEN = config.token;
 const PREFIX = config.prefix;
 var answerlist = ["The twin towers", "The peasants of casterly rock", "Fallout 4", "The dragonborn", "Six and a half american dollars", "The Chosen One", "Link", "The Hero of time", "The Hero of Rhyme", "Edwin Vancleef", "Ezio", "Some fuckin edgy guy", "Chell", "Shell", "GlaD0s", "Deckard", "A Fast food worker", "Gordon Freeman", "Talion", "Stanley", "Iron Chancellor Otto von Bismarck", "Doom Guy", "Mario", "Luigi", "That guy", "Kirby", "The Kazoo Kid", "Kratos", "KratOS", "Scorpion", "Johnny Cage", "Sub-Zero", "The man himself", "Han Solo", "Harrison Ford", "Lord Revan", "A Marine", "Iron Man", "Tony Stark", "Robert Downey Jr.", "Captain America", "The Econonmy", "Black Widow", "A Spider", "Spider man", "Spider-man", "Spiderman", "Thor", "Chris Hemsworth", "Loki", "Legolas", "Bilbo Baggins", "Frodo", "Falcon", "Hawkeye", "Ant Man", "Paul Rudd", "Thanos", "Galactus", "The Silver Surfer", "Stan Lee", "Mr. Bean", "My neighbor steve", "A mysterious man you've never seen in your life", "Jake Paul", "Pewdiepie", "Bilbo Baggins", "A ninja that was hiding in a corner", "Kim Jung Un", "Kim Jung OOF", "Kim Jung-Possible", "Franklin Deleanor Roosevelt", "Donald Trump", "Bill Gates", "Steven Hawking", "The creator of the Note 5", "Heman", "Actual Cannibal Shia LeBouf", "Shia LeBouf", "Mia Khalifa", "National Geographic", "Percy Jackson", "a boosted monkey", "someone who is clearly cheating", "Barry B. Benson", "Jerry Seinfeild", "Bill Clinton", "Bane", "Danny DeVito"];
@@ -111,7 +113,11 @@ function russianActive(input) {
     }
   });
 }
-
+function translater(textinput, lang) {
+  const deeplapi = "https://api-free.deepl.com/v2/translate?auth_key=" + DEEPLT + "&text=" + textinput + "&target_lang=" + lang
+  const response = await fetch(deeplapi);
+  console.log(response);
+}
 bot.on("message", async message => {
   console.log(message.content);
 
@@ -1422,6 +1428,34 @@ bot.on("message", async message => {
       });
       //https://www.youtube.com/watch?v=8o7I69O1RzE
       break;
+    case "translate":
+      args.split()
+      langd = args[0];
+      inputtext = args.join(' ');
+      switch(langd):
+        case "DE":
+          translater(inputtext, "DE");
+          break;
+        case "EN":
+          break;
+        case "ES":
+          break;
+        case "FR":
+          break;
+        case "IT":
+          break;
+        case "JP":
+        case "JA":
+          break;
+        case "NL":
+          break;
+        case "PL":
+          break;
+        case "RU":
+          break;
+        case "CH":
+        case "ZH":
+          break;
     case "despair":
       var VC = message.member.voiceChannel;
       if (!VC)
